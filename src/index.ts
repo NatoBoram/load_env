1d9949887624fa8334ca11c48a8ebcb0b96d8148
Nato Boram <nato.boram+git@gmail.com>
2025-07-31 16:02:03 -0400

    ✨ Add support for secrets

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from "./env.ts"
 export * from "./load_env.ts"
+export * from "./secret.ts"
 export type * from "./loaded_env.ts"
---
+++ src/secret.ts
@@ -0,0 +1,311 @@
+import type { UUID } from "node:crypto"
+import { access, readFile } from "node:fs/promises"
+import { toBool } from "./boolean.ts"
+import { isUuid } from "./uuid.ts"
+
+async function maybeSecret(key: string): Promise<string | undefined> {
+	return secret(key).catch(() => undefined)
+}
+
+/**
+ * Obtains an optional secret as a boolean.
+ */
+export async function maybeSecretBool(
+	key: string,
+): Promise<boolean | undefined> {
+	const str = await maybeSecret(key)
+	if (!str) return undefined
+
+	return toBool(key, str)
+}
+
+export async function maybeSecretDate(key: string): Promise<Date | undefined> {
+	const str = await maybeSecret(key)
+	if (!str) return undefined
+
+	const date = new Date(str)
+	if (isNaN(date.getTime()))
+		throw new Error(`$${key} is not a valid Date: ${str}`)
+	return date
+}
+
+/**
+ * Obtains an optional secret as a floating-point number.
+ */
+export async function maybeSecretFloat(
+	key: string,
+): Promise<number | undefined> {
+	const str = await maybeSecret(key)
+	if (str === undefined) return undefined
+
+	const num = parseFloat(str)
+	if (isNaN(num)) throw new Error(`$${key} is not a number: ${str}`)
+	return num
+}
+
+/**
+ * Obtains an optional secret as an integer.
+ *
+ * @example
+ *
+ * export const PORT = maybeSecretInt("PORT")
+ */
+export async function maybeSecretInt(key: string): Promise<number | undefined> {
+	const str = await maybeSecret(key)
+	if (str === undefined) return undefined
+
+	const num = parseInt(str)
+	if (isNaN(num)) throw new Error(`$${key} is not a number: ${str}`)
+	return num
+}
+
+/**
+ * Obtains an optional secret as a string.
+ *
+ * @example
+ *
+ * export const NODE_ENV = maybeSecretString("NODE_ENV")
+ */
+export async function maybeSecretString(
+	key: string,
+): Promise<string | undefined> {
+	const str = await maybeSecret(key)
+	if (str === undefined) return undefined
+	return str
+}
+
+export async function maybeSecretStrings(
+	key: string,
+): Promise<string[] | undefined> {
+	const str = await maybeSecret(key)
+	if (str === undefined) return undefined
+
+	return str
+		.split(",")
+		.map(s => s.trim())
+		.filter(Boolean)
+}
+
+export async function maybeSecretUrl(key: string): Promise<URL | undefined> {
+	const str = await maybeSecret(key)
+	if (str === undefined) return undefined
+
+	try {
+		return new URL(str)
+	} catch (error) {
+		throw new Error(`$${key} is not a URL: ${str}`, { cause: error })
+	}
+}
+
+/**
+ * Obtains an optional secret as a UUID.
+ *
+ * @example
+ *
+ * export const TOKEN = maybeSecretUuid("TOKEN")
+ */
+export async function maybeSecretUuid(key: string): Promise<UUID | undefined> {
+	const str = await maybeSecret(key)
+	if (!str) return undefined
+	if (!isUuid(str)) throw new Error(`$${key} is not a UUID: ${str}`)
+	return str
+}
+
+async function secret(key: string): Promise<string> {
+	const path = process.env[key]?.trim() || `/run/secrets/${key}`
+
+	const accessed = await access(path).catch((error: unknown) => {
+		return new Error(`Couldn't access secret at "${path}"`, {
+			cause: { error, key, path },
+		})
+	})
+	if (accessed instanceof Error) throw accessed
+
+	const content = await readFile(path, "utf8")
+		.then(content => content.trim())
+		.catch((error: unknown) => {
+			return new Error(`Couldn't read secret at "${path}"`, {
+				cause: { error, key, path },
+			})
+		})
+	if (content instanceof Error) throw content
+
+	if (!content) {
+		if (!process.env[key]?.trim()) {
+			throw new Error(`$${key} is missing`, {
+				cause: { content, key, path },
+			})
+		}
+
+		throw new Error(`The secret at "${path}" is empty`, {
+			cause: { content, key, path },
+		})
+	}
+
+	return content
+}
+
+/**
+ * Obtains a secret as a boolean.
+ *
+ * @example
+ *
+ * export const CI = secretBool("CI", false)
+ */
+export async function secretBool(
+	key: string,
+	fallback?: boolean,
+): Promise<boolean> {
+	if (fallback !== undefined) {
+		const value = await maybeSecretBool(key)
+		if (value !== undefined) return value
+		return fallback
+	}
+
+	const value = await secret(key)
+	if (!value) throw new Error(`$${key} is missing`)
+
+	return toBool(key, value)
+}
+
+export async function secretDate(key: string, fallback?: Date): Promise<Date> {
+	if (fallback) {
+		const date = await maybeSecretDate(key)
+		if (date) return date
+		return fallback
+	}
+
+	const str = await secret(key)
+	if (!str) throw new Error(`$${key} is missing`)
+
+	const date = new Date(str)
+	if (isNaN(date.getTime()))
+		throw new Error(`$${key} is not a valid Date: ${str}`)
+
+	return date
+}
+
+/**
+ * Obtains a secret as a floating-point number.
+ *
+ * @example
+ *
+ * export const ZOOM = secretFloat("ZOOM", 1.0)
+ */
+export async function secretFloat(
+	key: string,
+	fallback?: number,
+): Promise<number> {
+	if (fallback !== undefined) {
+		const num = await maybeSecretFloat(key)
+		if (num !== undefined) return num
+		return fallback
+	}
+
+	const str = await secret(key)
+	if (!str) throw new Error(`$${key} is missing`)
+
+	const num = parseFloat(str)
+	if (isNaN(num)) throw new Error(`$${key} is not a number: ${str}`)
+	return num
+}
+
+/**
+ * Obtains a secret as an integer.
+ *
+ * @example
+ *
+ * export const PORT = secretInt("PORT", 3000)
+ */
+export async function secretInt(
+	key: string,
+	fallback?: number,
+): Promise<number> {
+	if (fallback !== undefined) {
+		const num = await maybeSecretInt(key)
+		if (num !== undefined) return num
+		return fallback
+	}
+
+	const str = await secret(key)
+	if (!str) throw new Error(`$${key} is missing`)
+
+	const num = parseInt(str)
+	if (isNaN(num)) throw new Error(`$${key} is not a number: ${str}`)
+	return num
+}
+
+/**
+ * Obtains a secret as a string.
+ *
+ * @example
+ *
+ * export const NODE_ENV = secretString("NODE_ENV", "development")
+ */
+export async function secretString(
+	key: string,
+	fallback?: string,
+): Promise<string> {
+	if (fallback !== undefined) {
+		const str = await maybeSecretString(key)
+		if (str !== undefined) return str
+		return fallback
+	}
+
+	const str = await secret(key)
+	if (!str) throw new Error(`$${key} is missing`)
+	return str
+}
+
+export async function secretStrings(
+	key: string,
+	fallback?: string[],
+): Promise<string[]> {
+	if (fallback) {
+		const strings = await maybeSecretStrings(key)
+		if (strings) return strings
+		return fallback
+	}
+
+	const str = await secret(key)
+	if (!str) throw new Error(`$${key} is missing`)
+
+	return str
+		.split(",")
+		.map(s => s.trim())
+		.filter(Boolean)
+}
+
+export async function secretUrl(key: string, fallback?: URL): Promise<URL> {
+	if (fallback) {
+		const url = await maybeSecretUrl(key)
+		if (url) return url
+		return fallback
+	}
+
+	const str = await secret(key)
+	try {
+		return new URL(str)
+	} catch (error) {
+		throw new Error(`$${key} is not a URL: ${str}`, { cause: error })
+	}
+}
+
+/**
+ * Obtains a secret as a UUID.
+ *
+ * @example
+ *
+ * export const TOKEN = secretUuid("TOKEN")
+ */
+export async function secretUuid(key: string, fallback?: UUID): Promise<UUID> {
+	if (isUuid(fallback)) {
+		const str = await maybeSecretUuid(key)
+		if (str) return str
+		return fallback
+	}
+
+	const str = await secret(key)
+	if (!isUuid(str)) throw new Error(`$${key} is not a UUID: ${str}`)
+	return str
+}
